8000dc32ca2e76d600a4c6b0ba86aa04d6d8ebc5
Joshua Kemp <josh.kemp@ziplist.com>
2014-04-18 10:52:06 -0400

    Using lodash library for the first time

--- tester.js
+++ tester.js
@@ -1,12 +1,7 @@
-var _ = require('lodash-node');
-
-
-
+var _ = require('lodash');
 
 var last = _.findLast([1, 2, 3, 4], function (num) {
-  return num % 2 == 1;
+  return num % 2 === 1;
 });
 
 console.log(last);
-
-
---
+++ type.js
@@ -0,0 +1,4 @@
+var _ = require('lodash');
+
+// console.log( typeof _( [] ).reverse );
+console.log(String(_([1, 2, 3]).reverse()));

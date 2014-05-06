8224f06144ce1d1735aa37211ec58a59958544ef
Joshua Kemp <josh.kemp@ziplist.com>
2014-05-06 17:23:21 -0400

    Udacity Unit 2 quizzes

---
+++ new_factorial.js
@@ -0,0 +1,10 @@
+function factorial(input) {
+  if (input === 0) {
+    return 1;
+  }
+    else {
+      return input * factorial(input - 1);
+    }
+}
+
+console.log(factorial(9));
--- new_loop.js
+++ new_loop.js
@@ -4,9 +4,13 @@ function print_numbers(input) {
 
   var counter = 1;
 
+
   while (counter <= input) {
     console.log(counter++);
   }
 }
 
 (print_numbers(numbers));
+
+
+

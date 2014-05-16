e59722b12bbfa4041b8728111d71639113d580ec
Joshua Kemp <josh.kemp@ziplist.com>
2014-05-16 13:10:34 -0400

    fix this commit

---
+++ super1.js
@@ -0,0 +1,18 @@
+function fix_machine(input_1, input_2) {
+  // var counter = 1;
+  // var counter_2 = 0;
+  // var check_1 = input_1
+  // var check_2 = input_2.slice(0);
+
+                      //5
+  // while (counter <= input_2.length) {
+
+
+  //   counter++;
+  // }
+
+
+
+  // return input_2;
+}
+console.log(fix_machine('jfkdsajkfdj', 'kjsaz'));
---
+++ superhero.js
@@ -0,0 +1,27 @@
+// # Write a Python procedure fix_machine to take 2 string inputs
+// # and returns the 2nd input string as the output if all of its
+// # characters can be found in the 1st input string and "Give me
+// # something that's not useless next time." if it's impossible.
+
+// // # NOTE: # If you are experiencing difficulties taking
+// //         # this problem seriously, please refer back to
+// //         # "Superhero flyby", the prequel, in Problem Set 11.
+
+// // # TOOLS: # if statement
+// //          # while loop
+// //          # string operations
+// //          # Unit 1 Basics
+
+function fix_machine(input_1, input_2) {
+
+
+
+
+  return input_2;
+}
+console.log(fix_machine('jfkdsajkfdj', 'kjsaz'));
+// console.log(fix_machine('wsx0-=mttrhix', 't-shirt'));
+
+
+// return "Give me
+// // # something that's not useless next time."

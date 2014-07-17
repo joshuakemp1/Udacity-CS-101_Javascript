8d54be1e722f2b36c16ba6918d3c05f68b65d699
Joshua Kemp <josh.kemp@ziplist.com>
2014-07-17 09:57:37 -0400

    Udacity Unit 3 assignment

---
+++ sum.js
@@ -0,0 +1,35 @@
+// # Define a procedure, sum_list,
+// # that takes as its input a
+// # list of numbers, and returns
+// # the sum of all the elements in
+// # the input list.
+
+
+
+function sum_list(input) {
+
+  var total = 0;
+
+   for (var i = 0; i < input.length; i++) {
+     total = input[i] + total;
+   }
+   return total;
+}
+
+console.log(sum_list([1, 7, 4, 67, 8, 12, 3, 5]));
+
+
+
+
+
+
+
+// #print sum_list([1, 7, 4])
+// #>>> 12
+
+// #print sum_list([9, 4, 10])
+// #>>> 23
+
+// #print sum_list([44, 14, 76])
+// #>>> 134
+
